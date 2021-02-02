a62831642b2ebeae885a4bb311cf5e79e8b98d66
madhof-ui <jeu_78@outlook.fr>
2021-02-02 15:27:41 +0100

    lien index.php avec bootstrap 4.5.3

--- index.php
+++ index.php
@@ -6,10 +6,11 @@ echo "je suis un test";
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="js/script.js"></script>
+	<meta charset="utf-8">
+	<title>Titre de la page</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<script src="js/script.js"></script>
 </head>
 <body>	
 	<div>
