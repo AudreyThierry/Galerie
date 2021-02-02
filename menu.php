e751bc845f871372d9e8ed4b4a5d6e27a351cbdf
Damien Varnier <damienvarnier76@gmail.com>
2021-02-02 13:49:01 +0100

    ajout cdn

--- menu.php
+++ menu.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <header>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   
@@ -20,3 +21,8 @@
   </div>
 </nav>
 </header>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        
+       
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
