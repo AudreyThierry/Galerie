31129f33b7d49273eba0d6c320a4373681b99169
madhof-ui <jeu_78@outlook.fr>
2021-02-10 15:39:25 +0100

    Intégration de l'overlay d'angel au click sur une image

---
+++ js/overlay.js
@@ -0,0 +1,72 @@
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+function on2() {
+  document.getElementById("overlay2").style.display = "block";
+}
+function off2() {
+  document.getElementById("overlay2").style.display = "none";
+}
+function on3() {
+  document.getElementById("overlay3").style.display = "block";
+}
+function off3() {
+  document.getElementById("overlay3").style.display = "none";
+}
+function on4() {
+  document.getElementById("overlay4").style.display = "block";
+}
+function off4() {
+  document.getElementById("overlay4").style.display = "none";
+}
+function on5() {
+  document.getElementById("overlay5").style.display = "block";
+}
+function off5() {
+  document.getElementById("overlay5").style.display = "none";
+}
+function on6() {
+  document.getElementById("overlay6").style.display = "block";
+}
+function off6() {
+  document.getElementById("overlay6").style.display = "none";
+}
+function on7() {
+  document.getElementById("overlay7").style.display = "block";
+}
+function off7() {
+  document.getElementById("overlay7").style.display = "none";
+}
+function on8() {
+  document.getElementById("overlay8").style.display = "block";
+}
+function off8() {
+  document.getElementById("overlay8").style.display = "none";
+}
+function on9() {
+  document.getElementById("overlay9").style.display = "block";
+}
+function off9() {
+  document.getElementById("overlay9").style.display = "none";
+}
+function on10() {
+  document.getElementById("overlay10").style.display = "block";
+}
+function off10() {
+  document.getElementById("overlay10").style.display = "none";
+}
+function on11() {
+  document.getElementById("overlay11").style.display = "block";
+}
+function off11() {
+  document.getElementById("overlay11").style.display = "none";
+}
+function on12() {
+  document.getElementById("overlay12").style.display = "block";
+}
+function off12() {
+  document.getElementById("overlay12").style.display = "none";
+}
