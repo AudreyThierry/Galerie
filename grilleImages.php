96f5af086ecd1fb3c62acc3c4f40e635da19e15c
madhof-ui <jeu_78@outlook.fr>
2021-02-10 14:09:27 +0100

    optimisation de la grille pour le reponsive

--- grilleImages.php
+++ grilleImages.php
@@ -1,16 +1,16 @@
 <div class="container" id="containerGallerie">
 	<h1 class="text-center text-uppercase text-lg-left mt-4">GALLERIE<small class="text-muted"> -  vacances</small></h1>
-	<div class="row">
-		<div class="col-lg-4">
+		<div class="row">
+		<div class="col-lg-4 col-md-4 col-6">
 			<img class="img-fluid mb-4" src="images/img1.jpg" alt="">
 		</div>
-		<div class="col-lg-4">
+		<div class="col-lg-4 col-md-4 col-6">
 			<img class="img-fluid mb-4" src="images/img2.jpg" alt="">
 		</div>
-		<div class="col-lg-4">
+		<div class="col-lg-4 col-md-4 col-6">
 			<img class="img-fluid mb-4" src="images/img3.jpg" alt="">
 		</div>
-		<div class="col-lg-4">
+		<div class="col-lg-4 col-md-4 col-6">
 			<img class="img-fluid mb-4" src="images/img4.jpg" alt="">
 		</div>
 	</div>
