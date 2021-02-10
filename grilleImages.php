cccc83c9ac0a7d81d665ceb58daa6eb2e614a19a
madhof-ui <jeu_78@outlook.fr>
2021-02-10 14:22:46 +0100

    Suprression des marges du navigateur par défaut

--- grilleImages.php
+++ grilleImages.php
@@ -1,7 +1,7 @@
 <div class="container" id="containerGallerie">
 	<h1 class="text-center text-uppercase text-lg-left mt-4">GALLERIE<small class="text-muted"> -  vacances</small></h1>
 	<div class="row">
-<div class="col-lg-3 col-md-4 col-6">
+		<div class="col-lg-3 col-md-4 col-6">
 			<img class="img-fluid mb-4" src="images/img1.jpg" alt="">
 		</div>
 		<div class="col-lg-3 col-md-4 col-6">
