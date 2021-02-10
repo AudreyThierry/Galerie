eca0abca5423a203d6551817622bdd2be0afceb2
madhof-ui <jeu_78@outlook.fr>
2021-02-10 14:19:40 +0100

    Ajout d'autres images

--- grilleImages.php
+++ grilleImages.php
@@ -1,17 +1,41 @@
 <div class="container" id="containerGallerie">
 	<h1 class="text-center text-uppercase text-lg-left mt-4">GALLERIE<small class="text-muted"> -  vacances</small></h1>
-		<div class="row">
-		<div class="col-lg-3 col-md-4 col-6">
+	<div class="row">
+<div class="col-lg-3 col-md-4 col-6">
 			<img class="img-fluid mb-4" src="images/img1.jpg" alt="">
 		</div>
 		<div class="col-lg-3 col-md-4 col-6">
-			<img class="img-fluid mb-4" src="images/img2.jpg" alt="">
+			<img class="img-fluid mb-4" src="images/img11.jpg" alt="">
 		</div>
 		<div class="col-lg-3 col-md-4 col-6">
 			<img class="img-fluid mb-4" src="images/img3.jpg" alt="">
 		</div>
 		<div class="col-lg-3 col-md-4 col-6">
 			<img class="img-fluid mb-4" src="images/img4.jpg" alt="">
+		</div>
+		<div class="col-lg-3 col-md-4 col-6">
+			<img class="img-fluid mb-4" src="images/img5.jpg" alt="">
+		</div>
+		<div class="col-lg-3 col-md-4 col-6">
+			<img class="img-fluid mb-4" src="images/img6.jpg" alt="">
+		</div>
+		<div class="col-lg-3 col-md-4 col-6">
+			<img class="img-fluid mb-4" src="images/img13.jpg" alt="">
+		</div>
+		<div class="col-lg-3 col-md-4 col-6">
+			<img class="img-fluid mb-4" src="images/img8.jpg" alt="">
+		</div>
+		<div class="col-lg-3 col-md-4 col-6">
+			<img class="img-fluid mb-4" src="images/img9.jpg" alt="">
+		</div>
+		<div class="col-lg-3 col-md-4 col-6">
+			<img class="img-fluid mb-4" src="images/img17.jpg" alt="">
+		</div>
+		<div class="col-lg-3 col-md-4 col-6">
+			<img class="img-fluid mb-4" src="images/img2.jpg" alt="">
+		</div>
+		<div class="col-lg-3 col-md-4 col-6">
+			<img class="img-fluid mb-4" src="images/img12.jpg" alt="">
 		</div>
 	</div>
 </div>
