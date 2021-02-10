cf5cc9fff4fc55159f42eb38d61aa977cde8ca26
madhof-ui <jeu_78@outlook.fr>
2021-02-10 14:07:07 +0100

    Test d'ajout de 4 images

--- grilleImages.php
+++ grilleImages.php
@@ -1,3 +1,17 @@
 <div class="container" id="containerGallerie">
 	<h1 class="text-center text-uppercase text-lg-left mt-4">GALLERIE<small class="text-muted"> -  vacances</small></h1>
+	<div class="row">
+		<div class="col-lg-4">
+			<img class="img-fluid mb-4" src="images/img1.jpg" alt="">
+		</div>
+		<div class="col-lg-4">
+			<img class="img-fluid mb-4" src="images/img2.jpg" alt="">
+		</div>
+		<div class="col-lg-4">
+			<img class="img-fluid mb-4" src="images/img3.jpg" alt="">
+		</div>
+		<div class="col-lg-4">
+			<img class="img-fluid mb-4" src="images/img4.jpg" alt="">
+		</div>
+	</div>
 </div>
